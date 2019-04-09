a1c7aaaa59d3f1df7d49ff77c558b7b2b610096f
Josh <grafton.josh@gmail.com>
2019-04-09 09:52:43 -0600

    practiced lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild,
+
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +23,47 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   @Input('svrElement') element: {type: string, name:string, content: string}
+  @Input() name: string
+  @ViewChild('heading') header: ElementRef
+  @ContentChild('contentParagraph') paragraph: ElementRef
 
-  constructor() { }
-
+  constructor() {
+    console.log('Constructor Called!')
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges')
+    console.log('Changes>>>', changes)
+  }
   ngOnInit() {
+    console.log('ngOnInit Called!')
+    console.log('Text Content>>>',this.header.nativeElement.textContent)
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck Called')
+  }
+  ngAfterContentInit(){
+    console.log('AfterContentInit')
+    console.log('CONTENTINIT >>>>', this.paragraph.nativeElement.textContent)
+  }
+
+  ngAfterContentChecked(){
+    console.log('AfterContentChecked')
+  }
+
+  ngAfterViewInit(){
+    console.log('AfterViewInit')
+    console.log('Text Content>>>',this.header.nativeElement.textContent)
+  }
+
+  ngAfterViewChecked(){
+    console.log('AfterViewChecked')
+  }
+
+  ngOnDestroy(){
+    console.log('OnDestroy')
   }
 
 }
